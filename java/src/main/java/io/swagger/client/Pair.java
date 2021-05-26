11aaae44a8b85832b37a54d929d2eafc132a449a
Sel_146093240078 <selenium.swagger+Sel_146093240078@gmail.com>
2021-05-26 09:32:45 +0000

    Testing_Commit_Message

--- java/src/main/java/io/swagger/client/Pair.java
+++ java/src/main/java/io/swagger/client/Pair.java
@@ -12,7 +12,7 @@
 
 package io.swagger.client;
 
-@javax.annotation.Generated(value = "io.swagger.codegen.v3.generators.java.JavaClientCodegen", date = "2021-05-26T09:31:27.008Z[GMT]")public class Pair {
+@javax.annotation.Generated(value = "io.swagger.codegen.v3.generators.java.JavaClientCodegen", date = "2021-05-26T09:32:44.480Z[GMT]")public class Pair {
     private String name = "";
     private String value = "";
 
--- java/src/main/java/io/swagger/client/StringUtil.java
+++ java/src/main/java/io/swagger/client/StringUtil.java
@@ -12,7 +12,7 @@
 
 package io.swagger.client;
 
-@javax.annotation.Generated(value = "io.swagger.codegen.v3.generators.java.JavaClientCodegen", date = "2021-05-26T09:31:27.008Z[GMT]")public class StringUtil {
+@javax.annotation.Generated(value = "io.swagger.codegen.v3.generators.java.JavaClientCodegen", date = "2021-05-26T09:32:44.480Z[GMT]")public class StringUtil {
   /**
    * Check if the given array contains the given value (with case-insensitive comparison).
    *
