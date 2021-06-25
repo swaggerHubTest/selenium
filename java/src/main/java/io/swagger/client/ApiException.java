6677ba1211487843fc044b6fbafd7e5841dfbed4
Sel_176085954615 <selenium.swagger+Sel_176085954615@gmail.com>
2021-06-25 08:59:57 +0000

    Push from SwaggerHub.

--- java/src/main/java/io/swagger/client/ApiException.java
+++ java/src/main/java/io/swagger/client/ApiException.java
@@ -15,7 +15,7 @@ package io.swagger.client;
 import java.util.Map;
 import java.util.List;
 
-@javax.annotation.Generated(value = "io.swagger.codegen.v3.generators.java.JavaClientCodegen", date = "2021-06-25T08:53:12.664Z[GMT]")public class ApiException extends Exception {
+@javax.annotation.Generated(value = "io.swagger.codegen.v3.generators.java.JavaClientCodegen", date = "2021-06-25T08:59:56.271Z[GMT]")public class ApiException extends Exception {
     private int code = 0;
     private Map<String, List<String>> responseHeaders = null;
     private String responseBody = null;
--- java/src/main/java/io/swagger/client/auth/ApiKeyAuth.java
+++ java/src/main/java/io/swagger/client/auth/ApiKeyAuth.java
@@ -17,7 +17,7 @@ import io.swagger.client.Pair;
 import java.util.Map;
 import java.util.List;
 
-@javax.annotation.Generated(value = "io.swagger.codegen.v3.generators.java.JavaClientCodegen", date = "2021-06-25T08:53:12.664Z[GMT]")public class ApiKeyAuth implements Authentication {
+@javax.annotation.Generated(value = "io.swagger.codegen.v3.generators.java.JavaClientCodegen", date = "2021-06-25T08:59:56.271Z[GMT]")public class ApiKeyAuth implements Authentication {
   private final String location;
   private final String paramName;
 
