8a278381fd35fb4b23e5ad57056f91b023a7918e
Sel_189101824535 <selenium.swagger+Sel_189101824535@gmail.com>
2021-07-08 10:18:31 +0000

    Push from SwaggerHub.

--- java/src/main/java/io/swagger/client/auth/ApiKeyAuth.java
+++ java/src/main/java/io/swagger/client/auth/ApiKeyAuth.java
@@ -17,7 +17,7 @@ import io.swagger.client.Pair;
 import java.util.Map;
 import java.util.List;
 
-@javax.annotation.Generated(value = "io.swagger.codegen.v3.generators.java.JavaClientCodegen", date = "2021-07-08T08:53:20.825Z[GMT]")public class ApiKeyAuth implements Authentication {
+@javax.annotation.Generated(value = "io.swagger.codegen.v3.generators.java.JavaClientCodegen", date = "2021-07-08T10:18:29.846Z[GMT]")public class ApiKeyAuth implements Authentication {
   private final String location;
   private final String paramName;
 
