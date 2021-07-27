9850f2c0b3ff617f90f77361a10b252628012908
Sel_208111201633 <selenium.swagger+Sel_208111201633@gmail.com>
2021-07-27 11:12:08 +0000

    Push from SwaggerHub.

--- java/src/main/java/io/swagger/client/ApiException.java
+++ java/src/main/java/io/swagger/client/ApiException.java
@@ -15,7 +15,7 @@ package io.swagger.client;
 import java.util.Map;
 import java.util.List;
 
-@javax.annotation.Generated(value = "io.swagger.codegen.v3.generators.java.JavaClientCodegen", date = "2021-07-27T10:44:31.559Z[GMT]")public class ApiException extends Exception {
+@javax.annotation.Generated(value = "io.swagger.codegen.v3.generators.java.JavaClientCodegen", date = "2021-07-27T11:12:06.511Z[GMT]")public class ApiException extends Exception {
     private int code = 0;
     private Map<String, List<String>> responseHeaders = null;
     private String responseBody = null;
