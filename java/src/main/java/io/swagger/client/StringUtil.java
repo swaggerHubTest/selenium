06c88bc94409438759b98b2d413f63674fcbe3c1
Sel_215143616167 <selenium.swagger+Sel_215143616167@gmail.com>
2021-08-03 14:36:20 +0000

    Testing_Commit_Message

--- java/src/main/java/io/swagger/client/StringUtil.java
+++ java/src/main/java/io/swagger/client/StringUtil.java
@@ -12,7 +12,7 @@
 
 package io.swagger.client;
 
-@javax.annotation.Generated(value = "io.swagger.codegen.v3.generators.java.JavaClientCodegen", date = "2021-08-03T14:20:25.216Z[GMT]")public class StringUtil {
+@javax.annotation.Generated(value = "io.swagger.codegen.v3.generators.java.JavaClientCodegen", date = "2021-08-03T14:36:18.331Z[GMT]")public class StringUtil {
   /**
    * Check if the given array contains the given value (with case-insensitive comparison).
    *
--- java/src/main/java/io/swagger/client/auth/OAuth.java
+++ java/src/main/java/io/swagger/client/auth/OAuth.java
@@ -17,7 +17,7 @@ import io.swagger.client.Pair;
 import java.util.Map;
 import java.util.List;
 
-@javax.annotation.Generated(value = "io.swagger.codegen.v3.generators.java.JavaClientCodegen", date = "2021-08-03T14:20:25.216Z[GMT]")public class OAuth implements Authentication {
+@javax.annotation.Generated(value = "io.swagger.codegen.v3.generators.java.JavaClientCodegen", date = "2021-08-03T14:36:18.331Z[GMT]")public class OAuth implements Authentication {
   private String accessToken;
 
   public String getAccessToken() {
