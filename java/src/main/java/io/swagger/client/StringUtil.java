6b76f24a831f02ca5e3273e999dd160e709f42f1
Sel_238120927961 <selenium.swagger+Sel_238120927961@gmail.com>
2021-08-26 12:09:35 +0000

    Testing_Commit_Message

--- java/src/main/java/io/swagger/client/StringUtil.java
+++ java/src/main/java/io/swagger/client/StringUtil.java
@@ -12,7 +12,7 @@
 
 package io.swagger.client;
 
-@javax.annotation.Generated(value = "io.swagger.codegen.v3.generators.java.JavaClientCodegen", date = "2021-08-26T12:08:18.437Z[GMT]")public class StringUtil {
+@javax.annotation.Generated(value = "io.swagger.codegen.v3.generators.java.JavaClientCodegen", date = "2021-08-26T12:09:33.067Z[GMT]")public class StringUtil {
   /**
    * Check if the given array contains the given value (with case-insensitive comparison).
    *
