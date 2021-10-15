d54e322749f1ff39b11c9d4b9b053ad6baa9a79b
Sel_288125005822 <selenium.swagger+Sel_288125005822@gmail.com>
2021-10-15 12:50:11 +0000

    Testing_Commit_Message

--- java/src/main/java/io/swagger/client/Configuration.java
+++ java/src/main/java/io/swagger/client/Configuration.java
@@ -12,7 +12,7 @@
 
 package io.swagger.client;
 
-@javax.annotation.Generated(value = "io.swagger.codegen.v3.generators.java.JavaClientCodegen", date = "2021-10-15T12:49:11.373Z[GMT]")public class Configuration {
+@javax.annotation.Generated(value = "io.swagger.codegen.v3.generators.java.JavaClientCodegen", date = "2021-10-15T12:50:08.273Z[GMT]")public class Configuration {
     private static ApiClient defaultApiClient = new ApiClient();
 
     /**
