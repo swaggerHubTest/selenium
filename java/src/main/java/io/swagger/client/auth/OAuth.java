59df7cf258c631dd95b4d58ef632ad8502ce0966
Sel_307230613889 <selenium.swagger+Sel_307230613889@gmail.com>
2021-11-03 23:06:18 +0000

    Push from SwaggerHub.

--- java/src/main/java/io/swagger/client/auth/OAuth.java
+++ java/src/main/java/io/swagger/client/auth/OAuth.java
@@ -17,7 +17,7 @@ import io.swagger.client.Pair;
 import java.util.Map;
 import java.util.List;
 
-@javax.annotation.Generated(value = "io.swagger.codegen.v3.generators.java.JavaClientCodegen", date = "2021-11-03T12:43:26.300Z[GMT]")public class OAuth implements Authentication {
+@javax.annotation.Generated(value = "io.swagger.codegen.v3.generators.java.JavaClientCodegen", date = "2021-11-03T23:06:15.774Z[GMT]")public class OAuth implements Authentication {
   private String accessToken;
 
   public String getAccessToken() {
