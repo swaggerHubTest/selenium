16c3ddbbaa980e1b28391c832163cce4b4c1ec73
Sel_334022118261 <selenium.swagger+Sel_334022118261@gmail.com>
2021-11-30 02:21:23 +0000

    Push from SwaggerHub.

--- java/src/main/java/io/swagger/client/Pair.java
+++ java/src/main/java/io/swagger/client/Pair.java
@@ -12,7 +12,7 @@
 
 package io.swagger.client;
 
-@javax.annotation.Generated(value = "io.swagger.codegen.v3.generators.java.JavaClientCodegen", date = "2021-11-30T01:04:41.484Z[GMT]")public class Pair {
+@javax.annotation.Generated(value = "io.swagger.codegen.v3.generators.java.JavaClientCodegen", date = "2021-11-30T02:21:20.210Z[GMT]")public class Pair {
     private String name = "";
     private String value = "";
 
--- java/src/main/java/io/swagger/client/auth/OAuth.java
+++ java/src/main/java/io/swagger/client/auth/OAuth.java
@@ -17,7 +17,7 @@ import io.swagger.client.Pair;
 import java.util.Map;
 import java.util.List;
 
-@javax.annotation.Generated(value = "io.swagger.codegen.v3.generators.java.JavaClientCodegen", date = "2021-11-30T01:04:41.484Z[GMT]")public class OAuth implements Authentication {
+@javax.annotation.Generated(value = "io.swagger.codegen.v3.generators.java.JavaClientCodegen", date = "2021-11-30T02:21:20.210Z[GMT]")public class OAuth implements Authentication {
   private String accessToken;
 
   public String getAccessToken() {
