a6609527dc845bd7bf8b422a0bc85387eeaed72f
Sel_67230012989 <selenium.swagger+Sel_67230012989@gmail.com>
2022-03-08 23:00:17 +0000

    Push from SwaggerHub.

--- java/src/main/java/io/swagger/client/Pair.java
+++ java/src/main/java/io/swagger/client/Pair.java
@@ -12,7 +12,7 @@
 
 package io.swagger.client;
 
-@javax.annotation.Generated(value = "io.swagger.codegen.v3.generators.java.JavaClientCodegen", date = "2022-03-08T22:56:03.009Z[GMT]")public class Pair {
+@javax.annotation.Generated(value = "io.swagger.codegen.v3.generators.java.JavaClientCodegen", date = "2022-03-08T23:00:14.441Z[GMT]")public class Pair {
     private String name = "";
     private String value = "";
 
