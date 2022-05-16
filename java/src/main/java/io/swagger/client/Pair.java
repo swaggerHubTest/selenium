8f83ef9e30649fddbfe9f92fde9bb65c5a1d32cd
Sel_136004823148 <selenium.swagger+Sel_136004823148@gmail.com>
2022-05-16 00:48:36 +0000

    Testing_Commit_Message

--- java/src/main/java/io/swagger/client/Pair.java
+++ java/src/main/java/io/swagger/client/Pair.java
@@ -12,7 +12,7 @@
 
 package io.swagger.client;
 
-@javax.annotation.Generated(value = "io.swagger.codegen.v3.generators.java.JavaClientCodegen", date = "2022-05-16T00:46:34.421Z[GMT]")public class Pair {
+@javax.annotation.Generated(value = "io.swagger.codegen.v3.generators.java.JavaClientCodegen", date = "2022-05-16T00:48:30.559Z[GMT]")public class Pair {
     private String name = "";
     private String value = "";
 
--- java/src/main/java/io/swagger/client/auth/ApiKeyAuth.java
+++ java/src/main/java/io/swagger/client/auth/ApiKeyAuth.java
@@ -17,7 +17,7 @@ import io.swagger.client.Pair;
 import java.util.Map;
 import java.util.List;
 
-@javax.annotation.Generated(value = "io.swagger.codegen.v3.generators.java.JavaClientCodegen", date = "2022-05-16T00:46:34.421Z[GMT]")public class ApiKeyAuth implements Authentication {
+@javax.annotation.Generated(value = "io.swagger.codegen.v3.generators.java.JavaClientCodegen", date = "2022-05-16T00:48:30.559Z[GMT]")public class ApiKeyAuth implements Authentication {
   private final String location;
   private final String paramName;
 
