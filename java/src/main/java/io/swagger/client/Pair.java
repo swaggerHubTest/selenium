374aded548f7923d0af19ed0b4cea1a4e1242e7c
Sel_179070024108 <selenium.swagger+Sel_179070024108@gmail.com>
2022-06-28 07:00:30 +0000

    Testing_Commit_Message

--- java/src/main/java/io/swagger/client/Pair.java
+++ java/src/main/java/io/swagger/client/Pair.java
@@ -12,7 +12,7 @@
 
 package io.swagger.client;
 
-@javax.annotation.Generated(value = "io.swagger.codegen.v3.generators.java.JavaClientCodegen", date = "2022-06-28T06:59:30.466Z[GMT]")public class Pair {
+@javax.annotation.Generated(value = "io.swagger.codegen.v3.generators.java.JavaClientCodegen", date = "2022-06-28T07:00:25.766Z[GMT]")public class Pair {
     private String name = "";
     private String value = "";
 
