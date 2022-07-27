06268cdba2ae72dda52e7d1d0470d9a39337e261
Sel_208152110084 <selenium.swagger+Sel_208152110084@gmail.com>
2022-07-27 15:21:22 +0000

    Testing_Commit_Message

--- java/README.md
+++ java/README.md
@@ -2,7 +2,7 @@
 
 Sample Application Flow OAuth2 Project
 - API version: 1.0.0
-  - Build date: 2022-07-27T15:19:28.431Z[GMT]
+  - Build date: 2022-07-27T15:21:15.789Z[GMT]
 
 This is an example of using OAuth2 Application Flow in a specification to describe security to your API.
 
--- java/src/main/java/io/swagger/client/Pair.java
+++ java/src/main/java/io/swagger/client/Pair.java
@@ -12,7 +12,7 @@
 
 package io.swagger.client;
 
-@javax.annotation.Generated(value = "io.swagger.codegen.v3.generators.java.JavaClientCodegen", date = "2022-07-27T15:19:28.431Z[GMT]")public class Pair {
+@javax.annotation.Generated(value = "io.swagger.codegen.v3.generators.java.JavaClientCodegen", date = "2022-07-27T15:21:15.789Z[GMT]")public class Pair {
     private String name = "";
     private String value = "";
 
